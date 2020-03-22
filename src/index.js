7afcd03ee1fdf5ce88a5a9434631daaa5a2f9080
Natallia Bialevich <natalibelevich94@gmail.com>
2020-03-22 18:34:28 +0300

    init: solve the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let min = array[0];
 
-exports.min = function min (array) {
-  return 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+
+        }
+    }
+    return min;
+};
+
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let max = array[0]
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+return max;
 }
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    let sum=0;
+    for (let i = 0; i < array.length; i++){
+        sum= sum + array[i];
+    }
+    let avr = sum/array.length;
+    return avr;
 }
